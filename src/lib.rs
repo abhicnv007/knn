4d64c9ac6a81d045183e7988e4faf96496d5d059
Abhishek N V <abhicnv007@gmail.com>
2020-05-22 01:40:09 +0530

    pointcloud holds a generic reference and not a Point anymore

--- src/lib.rs
+++ src/lib.rs
@@ -3,33 +3,37 @@
 mod heap;
 
 #[derive(Clone)]
-pub struct PointCloud<T> {
+pub struct PointCloud<'a, T> {
     // stores all points
-    points: Vec<T>,
+    points: Vec<&'a T>,
 
     // return the distance given 2 ponints
     dist_fn: fn(&T, &T) -> f64,
 }
 
-impl<T> PointCloud<T> {
-    pub fn new(dist_fn: fn(&T, &T) -> f64) -> PointCloud<T> {
-        PointCloud {
+impl<'a, T> PointCloud<'a, T> {
+    pub fn new(dist_fn: fn(&T, &T) -> f64) -> Self {
+        Self {
             points: Vec::new(),
             dist_fn: dist_fn,
         }
     }
 
-    pub fn add_point(&mut self, p: T) {
+    pub fn add_point(&mut self, p: &'a T) {
         self.points.push(p)
     }
 
-    pub fn get_nearest_n(&self, p: &T, n: usize) -> Vec<f64> {
+    pub fn get_nearest_n(&self, p: &T, n: usize) -> Vec<(f64, &T)> {
         let mut h = heap::Heap::new(n);
         for i in 0..self.points.len() {
-            let d = (self.dist_fn)(&self.points[i], p);
-            h.insert(d);
+            let d = (self.dist_fn)(self.points[i], p);
+            h.insert(d, &self.points[i]);
         }
-        h.get_elements()
+        let mut knn = Vec::new();
+        for i in h.get_elements() {
+            knn.push((i.0, *i.1));
+        }
+        knn
     }
 
     pub fn len(&self) -> usize {
