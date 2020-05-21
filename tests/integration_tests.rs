601a6f498b6296642f44794a5ca4fc2002e680b6
Abhishek N V <abhicnv007@gmail.com>
2020-05-21 23:46:58 +0530

    change tests to use generics

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,11 +1,29 @@
 extern crate pointcloud;
 
-use pointcloud::Point;
 use pointcloud::PointCloud;
+
+#[derive(Clone, Debug)]
+pub struct Point {
+    coords: [f64; 2],
+}
+
+impl Point {
+    pub fn new(c: [f64; 2]) -> Point {
+        Point { coords: c }
+    }
+}
+
+fn euclidean(p: &Point, q: &Point) -> f64 {
+    let mut sosq = 0.0;
+    for i in 0..p.coords.len() {
+        sosq += (p.coords[i] - q.coords[i]).powf(2.0)
+    }
+    sosq.sqrt()
+}
 
 #[test]
 fn add_point() {
-    let mut pc = PointCloud::new();
+    let mut pc = PointCloud::new(euclidean);
     // add 4 points
     let points = [[1.0, 1.0], [2.0, 2.0], [10.0, 5.0], [11.0, 15.0]];
     for i in 0..points.len() {
@@ -16,18 +34,18 @@ fn add_point() {
 
 #[test]
 fn test_get_nearest_n() {
-    let mut pc = PointCloud::new();
+    let mut pc = PointCloud::new(euclidean);
     pc.add_point(Point::new([2.0, 2.0]));
     pc.add_point(Point::new([2.0, 1.0]));
     pc.add_point(Point::new([3.0, 1.0]));
 
     let p = Point::new([1.0, 1.0]);
-    let (d, _) = pc.get_nearest_n(&p, 1);
+    let d = pc.get_nearest_n(&p, 1);
     // assert that it returns n points
     assert_eq!(d.len(), 1);
     assert_eq!(d[0], 1.0);
 
-    let (d, _) = pc.get_nearest_n(&p, 2);
+    let d = pc.get_nearest_n(&p, 2);
 
     let expected = vec![1.0, 2.0f64.sqrt()];
     assert_eq!(d.len(), 2);
